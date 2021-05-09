4408cf3907613d47c6d6cdc3490644dc7caaa81a
Samuel Novaes <samuel.novaes.96@gmail.com>
2021-05-09 18:48:15 -0300

    Set up user API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const app = express();
 app.use(compression());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', require('./routes/users.route'));
+app.use('/user', require('./routes/user.route'));
 
 module.exports = app;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const walletSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	}
+	username: String,
+	email: String,
+	firstName: String,
+	lastName: String,
+	password: String
 }, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
 
-module.exports = mongoose.model('wallet', walletSchema);
+walletSchema.statics.joiValidate = (body, field = null) => {
+	const keys = {
+		username: Joi.string().alphanum().min(6).required().label('Username'),
+		email: Joi.string().email().required().label('Email'),
+		firstName: Joi.string().alphanum().required().label('First name'),
+		lastName: Joi.string().alphanum().required().label('Last name'),
+		password: Joi.string().min(6).required().label('Password')
+	};
+	const schema = Joi.object().keys(field ? { [field]: keys[field] } : keys);
+	return schema.validateAsync(body, {
+		allowUnknown: true
+	});
+};
+
+module.exports = mongoose.model('user', walletSchema);
