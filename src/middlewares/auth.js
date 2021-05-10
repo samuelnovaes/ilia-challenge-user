a4c881399013bf8f1164e5200c83355f95880cbe
Samuel Novaes <samuel.novaes.96@gmail.com>
2021-05-09 21:30:17 -0300

    Revert "Verify if user exists where authenticating"
    This reverts commit 206f9b1f8e32bf4007339f555b7dcbab87b131f6.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,4 @@
 const CustomError = require('../utils/customError');
-const User = require('../models/user.model');
 const parseError = require('../utils/parseError');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
@@ -7,31 +6,20 @@ const { promisify } = require('util');
 module.exports = async (req, res, next) => {
 	try {
 		const authorization = req.get('Authorization');
-		if (!authorization) {
+		if(!authorization) {
 			throw new CustomError('The access token is required', 401);
 		}
-
 		const token = authorization.replace(/^bearer (.*)$/i, '$1');
-		let payload = null;
 		try {
-			payload = await promisify(jwt.verify)(token, process.env.PRIVATE_KEY);
+			const payload = await promisify(jwt.verify)(token, process.env.PRIVATE_KEY);
+			req.userId = payload._id;
+			next();
 		}
-		catch (error) {
-			throw new CustomError('Invalid access token', 401);
+		catch(error) {
+			throw new CustomError('Invalid access token');
 		}
-
-		const user = await User.findOne({
-			_id: payload._id
-		});
-		if (!user) {
-			throw new CustomError('User not found');
-		}
-		
-		req.userId = payload._id;
-
-		next();
 	}
-	catch (error) {
+	catch(error) {
 		parseError(error, res);
 	}
 };
