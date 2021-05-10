23ce153b7f12633aa94887b56b1bfb61c2dace18
Samuel Novaes <samuel.novaes.96@gmail.com>
2021-05-09 21:32:06 -0300

    Verify if user exists where authenticating

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,5 @@
 const CustomError = require('../utils/customError');
+const User = require('../models/user.model');
 const parseError = require('../utils/parseError');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
@@ -6,20 +7,31 @@ const { promisify } = require('util');
 module.exports = async (req, res, next) => {
 	try {
 		const authorization = req.get('Authorization');
-		if(!authorization) {
+		if (!authorization) {
 			throw new CustomError('The access token is required', 401);
 		}
+
 		const token = authorization.replace(/^bearer (.*)$/i, '$1');
+		let payload = null;
 		try {
-			const payload = await promisify(jwt.verify)(token, process.env.PRIVATE_KEY);
-			req.userId = payload._id;
-			next();
+			payload = await promisify(jwt.verify)(token, process.env.PRIVATE_KEY);
 		}
-		catch(error) {
-			throw new CustomError('Invalid access token');
+		catch (error) {
+			throw new CustomError('Invalid access token', 401);
 		}
+
+		const user = await User.findOne({
+			_id: payload._id
+		});
+		if (!user) {
+			throw new CustomError('User not found');
+		}
+		
+		req.userId = payload._id;
+
+		next();
 	}
-	catch(error) {
+	catch (error) {
 		parseError(error, res);
 	}
 };
